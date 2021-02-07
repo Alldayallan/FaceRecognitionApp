841d36e709cc430b06281245bf11f1b6aca8ca6a
Allan <mail@allanreid.co>
2021-02-07 11:47:43 -0800

    Initial Commit

---
+++ FaceRecognitionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceRecognitionApp
+//
+//  Created by Allan Reid on 2/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
